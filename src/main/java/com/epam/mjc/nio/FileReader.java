13b68fcf8612b0017ca6496b2b9f903c6c94a4bf
Jahongir04 <jahongirachilov004@gmail.com>
2023-12-28 15:10:06 +0500

    fixed all

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,24 @@
 package com.epam.mjc.nio;
 
-import java.io.File;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Map<String,String> map = new HashMap<>();
+        try(BufferedReader bufferedReader = new BufferedReader(new java.io.FileReader(file.getAbsolutePath()));){
+            String str;
+            while (bufferedReader.ready()) {
+                str = bufferedReader.readLine();
+                map.put(str.substring(0,str.indexOf(": ")),str.substring(str.indexOf(": ")+2));
+            }
+            return new Profile( map.get("Name"), Integer.valueOf(map.get("Age")), map.get("Email"), Long.valueOf(map.get("Phone")));
+        }
+        catch (IOException e) {
+            throw new MyOwnRuntimeException(e.getMessage());
+        }
     }
 }
---
+++ src/main/java/com/epam/mjc/nio/MyOwnRuntimeException.java
@@ -0,0 +1,7 @@
+package com.epam.mjc.nio;
+
+public class MyOwnRuntimeException extends RuntimeException {
+    public MyOwnRuntimeException(String message) {
+        super(message);
+    }
+}
